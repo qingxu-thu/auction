e65a17468a63fb74a57651f0f075f33873c0ec16
qingxu-thu <wanghaox19@mails.tsinghua.edu.cn>
2021-01-11 21:45:44 +0800

    orign

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import math
 import xlrd
 #q;v;mu;lda
 
+g = 0.5
+a_1 = 0.3
+a_3 = 0.3
+b = 0.3
+a = 0.5
+
 def N(q,v):
-    return g*q-a_3/3-a_1*v*v*q+b*v*q+1/2*q*q*v
+    return g*q-a_3/3*q*q*q-a_1*v*v*q+b*v*q+1/2*q*q*v
 
 def N_v(q,v):
     return (-2*a_1*q*v+v*q)
@@ -28,6 +34,15 @@ def I_cal(q,v,alpha,lda,rho):
     else:
         I = N(q,v)-N_v(q,v)/rho
     return I
+
+def I_q0(v):
+    return g - a_1*v*v + b*v
+
+def mu_max(v_list):
+    x = []
+    for i in range(v_list.shape[0]):
+        x.append(I_q0(v_list[i]))
+    return max(x)
 
 def inverse_I(mu,alpha,lda,v,rho):
     if alpha<lda:
@@ -55,7 +70,7 @@ def allocation(N,lda,mu_list,alpha_list,v_list,B_list,q_0,rho_list):
     return demand_acc, q, mu, q_acc
 
 
-def demand_satisfy(N,lda_list,mu_list,alpha_list,v_list,B_list,q_0,d,rho_list,vmax,vmin,flag):
+def demand_satisfy(N,lda_list,mu_list,alpha_list,v_list,B_list,q_0,d,rho_list,vmax,vmin,flag,interval=0):
     for lda in lda_list:
         v_list = rounding(N,alpha_list,lda,flag,v_list,interval,vmax,vmin)
         demand, q, mu, q_acc = allocation(N,lda,mu_list,alpha_list,v_list,B_list,q_0,rho_list)
@@ -90,14 +105,33 @@ def rho_cal(N,vmax,vmin,v_list):
 def read(name):
     data = xlrd.open_workbook(name)
     table = data.sheets()[0]
-    B = np.array(table.col_values(0), dtype = "float64")
-    alpha = np.array(table.col_values(1), dtype = "float64")
+    B = np.array(table.col_values(1), dtype = "float64")
+    alpha = np.array(table.col_values(0), dtype = "float64")
     #G = np.array(table.col_values(0), dtype = "float64")
     return B,alpha
 
+def q_quantile_cal(F_dis):
+
+
 if __name__ == "__main__":
     name = './data.xlsx'
-    B_list,alpha =  read(name)
-    v_list = 
-    
-    q,lda,R = demand_satisfy(N,lda_list,mu_list,alpha_list,v_list,B_list,q_0,d,rho_list,vmax,vmin,flag)
+    prop = 0.1
+    prop_2 = 0.55
+    B_list,alpha_list =  read(name)
+    lda_list = np.sort(alpha_list)
+    N = B_list.shape[0]
+    v_max = np.random.rand((N))*300
+    v_min = np.maximum(v_max-np.random.rand((N))*60,15)
+    v_list = np.random.rand((N))*(v_max-v_min)+v_min
+    rho_list = rho_cal(N,v_max,v_min,v_list)
+    mu_m = mu_max(v_list)
+    mu_list = np.linspace(0,mu_m,1000) 
+    q_0 = prop*np.sum(B_list)
+    d = (B_list+q_0)*prop_2
+    flag = 1
+    q,lda,R = demand_satisfy(N,lda_list,mu_list,alpha_list,v_list,B_list,q_0,d,rho_list,v_max,v_min,flag)
+
+    flag = 0
+
+
+    interval_size = 0.01*()
